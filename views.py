0b4b54d7fc35f82ebcb0ce9a7e8e20e10c3914c1
ioan beilic <ioan@ioans-MacBook-Pro.local>
2026-03-07 13:18:24 +0100

    feat: convert drawer panels to full-page forms

--- views.py
+++ views.py
@@ -3,6 +3,8 @@ Shipping & Delivery Module Views
 """
 from django.core.paginator import Paginator
 from django.db.models import Q, Count
+from django.http import HttpResponse
+from django.urls import reverse
 from django.shortcuts import get_object_or_404, render as django_render
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
@@ -111,6 +113,7 @@ def carriers_list(request):
     }
 
 @login_required
+@htmx_view('shipping/pages/carrier_add.html', 'shipping/partials/carrier_add_content.html')
 def carrier_add(request):
     hub_id = request.session.get('hub_id')
     if request.method == 'POST':
@@ -124,10 +127,13 @@ def carrier_add(request):
         obj.tracking_url = tracking_url
         obj.is_active = is_active
         obj.save()
-        return _render_carriers_list(request, hub_id)
-    return django_render(request, 'shipping/partials/panel_carrier_add.html', {})
+        response = HttpResponse(status=204)
+        response['HX-Redirect'] = reverse('shipping:carriers_list')
+        return response
+    return {}
 
 @login_required
+@htmx_view('shipping/pages/carrier_edit.html', 'shipping/partials/carrier_edit_content.html')
 def carrier_edit(request, pk):
     hub_id = request.session.get('hub_id')
     obj = get_object_or_404(Carrier, pk=pk, hub_id=hub_id, is_deleted=False)
@@ -138,7 +144,7 @@ def carrier_edit(request, pk):
         obj.is_active = request.POST.get('is_active') == 'on'
         obj.save()
         return _render_carriers_list(request, hub_id)
-    return django_render(request, 'shipping/partials/panel_carrier_edit.html', {'obj': obj})
+    return {'obj': obj}
 
 @login_required
 @require_POST
